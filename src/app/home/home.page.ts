7af14a813f5f90f48071af63f3ed10d1d7497b53
yahs3458 <yash.shailza1234@gmail.com>
2023-08-02 16:06:19 +0530

    login functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,25 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LoadingController, Platform, ToastController } from '@ionic/angular';
 import jsQR from 'jsqr';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  scanActive=true;
+  scanActive = true;
   scanResult: string | null = null;
-  @ViewChild('video', {static:false}) video: ElementRef | undefined;
-  @ViewChild('canvas', {static:false}) canvas: ElementRef | undefined;
+  @ViewChild('video', { static: false }) video: ElementRef | undefined;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef | undefined;
 
-  constructor(private toastCtrl: ToastController, private loadingCTRL:LoadingController, private plt: Platform) {
-    
+  constructor(private toastCtrl: ToastController, private loadingCTRL: LoadingController, private plt: Platform , private router: Router) {
+
     const isInStandaloneMode = () =>
-    'standalone' in window.navigator && window.navigator['standalone'];
-     
+      'standalone' in window.navigator && window.navigator['standalone'];
 
-    if(this.plt.is('ios') && isInStandaloneMode()){
+
+    if (this.plt.is('ios') && isInStandaloneMode()) {
       console.log('i am an ios PWA!');
     }
 
@@ -26,88 +27,89 @@ export class HomePage {
   }
 
 
-  videoElement:any;
-  canvasElement:any;
-  canvasContext:any;
+  videoElement: any;
+  canvasElement: any;
+  canvasContext: any;
   loading: HTMLIonLoadingElement | null = null;
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.videoElement = this.video?.nativeElement;
     this.canvasElement = this.canvas?.nativeElement;
     this.canvasContext = this.canvasElement.getContext('2d');
 
   }
-  async startScan(){
-  const stream =await  navigator.mediaDevices.getUserMedia({
-    video:{facingMode:'environment'}
-  });
-  this.videoElement.srcObject = stream;
-  this.videoElement.setAttribute('playsinline',true)
-  this.videoElement.play();
-  this.loading = await this.loadingCTRL.create({});
-  await this.loading.present();
-  requestAnimationFrame(this.scan.bind(this));
+  async startScan() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+    this.videoElement.srcObject = stream;
+    this.videoElement.setAttribute('playsinline', true)
+    this.videoElement.play();
+    this.loading = await this.loadingCTRL.create({});
+    await this.loading.present();
+    requestAnimationFrame(this.scan.bind(this));
   };
- async scan(){
-  console.log('scan');
-    if(this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA){
-       if(this.loading){
+  async scan() {
+    console.log('scan');
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
         await this.loading.dismiss();
         this.loading = null;
-        this.scanActive=true;
-       }
-       this.canvasElement.height = this.videoElement.videoHeight;
-       this.canvasElement.width = this.videoElement.videoWidth;
+        this.scanActive = true;
+      }
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
 
-       this.canvasContext.drawImage(
+      this.canvasContext.drawImage(
         this.videoElement,
         0,
         0,
         this.canvasElement.width,
         this.canvasElement.height
-       );
+      );
 
-       const imageData = this.canvasContext.getImageData(
-         0,
-         0,
-         this.canvasElement.width,
-         this.canvasElement.height
-       );
-       const code = jsQR(imageData.data,imageData.width,imageData.height ,{
-        inversionAttempts:'dontInvert'
-       });
-       console.log('code:' , code);
-       if(code){
-        this.scanActive=false;
-        this.scanResult=code.data;
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+      console.log('code:', code);
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
         this.showQrToast();
-       } else {
-        if(this.scanActive){
-       requestAnimationFrame(this.scan.bind(this));}
-       }
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
 
 
 
-    }else{
+    } else {
       requestAnimationFrame(this.scan.bind(this));
     }
 
   }
-  
-  stopScan(){
-    this.scanActive=false;
+
+  stopScan() {
+    this.scanActive = false;
 
   };
-  reset(){ 
-    this.scanResult=null;
+  reset() {
+    this.scanResult = null;
   };
-  async showQrToast(){
+  async showQrToast() {
     const toast = await this.toastCtrl.create({
-      message:`Open ${this.scanResult}?`,
+      message: `Open ${this.scanResult}?`,
       position: 'top',
       buttons: [
         {
-          text:'Open',
+          text: 'Open',
           handler: () => {
             if (this.scanResult) {
               window.open(this.scanResult, '_system', 'location=yes');
@@ -117,5 +119,6 @@ export class HomePage {
       ]
     });
     toast.present();
+    this.router.navigate(['/log-in'], { queryParams: { username: this.scanResult } });
   }
 }
--- src/app/log-in/log-in.page.ts
+++ src/app/log-in/log-in.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.page.html',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router,private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
+  username!: string;
+  password!: string;
+  showPassword: boolean | undefined;
 
+
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  ionViewWillEnter() {
+    // Retrieve the username from the query parameter
+    this.username = this.route.snapshot.queryParams['username'];
+  }
+ 
+  onLogin(){
+    console.log('Logging in...');
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    this.router.navigate(['/pendingDashboard']);
+  }
 }
+
+
