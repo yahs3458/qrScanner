5acfeb5f75526b3cc5c5b8847db34cc605fc2891
yahs3458 <yash.shailza1234@gmail.com>
2023-10-10 16:11:01 +0530

    logo given

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -85,23 +85,16 @@ export class HomePage {
       if (code) {
         this.scanActive = false;
         this.scanResult = code.data;
-        this.showQrToast();
       } else {
         if (this.scanActive) {
           requestAnimationFrame(this.scan.bind(this));
         }
       }
-
-
-
     } else {
       requestAnimationFrame(this.scan.bind(this));
     }
-
   }
-
   stopScan() {
-    
     if (this.videoElement && this.videoElement.srcObject) {
       const stream = this.videoElement.srcObject as MediaStream;
       const tracks = stream.getTracks();
@@ -118,7 +111,6 @@ export class HomePage {
   };
   
   async showQrToast() {
-
     this.router.navigate(['/login'],
      { queryParams: { userName: this.scanResult } });
 }
